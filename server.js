80faa89c34d44c0c3595598a76ab8f96265ead62
meteoben <meteoben@gmail.com>
2024-01-19 11:07:26 +0100

    commit serveur api call ok

--- server.js
+++ server.js
@@ -7,10 +7,15 @@ app.use(cors())
 // app.get("/", function(req, res) {
 //   res.send("It's working!")
 // })
-app.get("/", function(req, res) {
-    res.send({"name": "Jane Doe"}) // Should be json format
-  })
+// app.get("/", function(req, res) {
+//     res.send({"name": "Jane Doe"}) // Should be json format
+//   })
 
-app.listen(3000, () => {
+app.listen(8080, () => {
   console.log("app listening on port 3000")
 })
+
+app.get('/', (req, res) => {
+    res.send('Hello from our server!')
+})
+
